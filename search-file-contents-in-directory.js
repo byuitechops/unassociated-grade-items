623163499a151bf728ea3f3bfa7a4e09352ecdcc
ajshiff <ajshiffler@gmail.com>
2018-07-03 18:15:44 -0600

    search-file-contents-in-directory.js now prints out all files found in the directory and all subdirectories as long as it has a '.' extension.

--- search-file-contents-in-directory.js
+++ search-file-contents-in-directory.js
@@ -1,78 +1,50 @@
 const fs = require('fs');
 const asyncLib = require('async');
-const targetDirectory = `C:\\Users\\ajshiff\\Documents\\git\\d2l-to-canvas\\unassociated-grade-items\\node_modules\\child-development-kit\\factory\\unzipped\\Conversion Test Gauntlet 1\\`;
+// const targetDirectory = `C:\\Users\\ajshiff\\Documents\\git\\d2l-to-canvas\\unassociated-grade-items\\node_modules\\child-development-kit\\factory\\unzipped\\Conversion Test Gauntlet 1\\`;
+const targetDirectory = process.argv[2];
 const phrase = 'grade';
 //closures
 
 /********************************************************************
- * filesInDirectory() relies on closures to allow a directoryLocation
- * to be utilized by a function that will be used in the main waterfall.
- * It fetches all items in a given directory and organizes them into two
- * arrays of files and subdirectories. An object of those arrays is passed
- * to the next function in the waterfall.
+ * 
  ********************************************************************/
-// filesInDirectory = function (directoryLocation) {
-    // return 
-    function filesInDirectory (getFiles_cb){
-        var fileArray = [];
-        var directoryArray = [];
-        fs.readdir(targetDirectory, function(err, files){
-            if (err){
-                getFiles_cb(err, files);
-            } else {
-                files.forEach(function(file){
-                    if(file.includes('.')) {
-                        fileArray.push(file);
-                    } else {
-                        directoryArray.push(file);
-                    }
-                });
-            }
-            // console.log(fileArray + '\n' + directoryArray)
-            getFiles_cb(null, {files: fileArray, directories: directoryArray});
+function readDirectory(allFiles, directories, getFiles_cb) {
+    if (directories.length !== 0) {
+        console.log(directories.length);
+        let newDirectories = [];
+        directories.forEach(directory => {
+            let files = fs.readdirSync(directory);
+            // console.log(files);
+            files.forEach(file => {
+                if(file.includes('.')) {
+                    allFiles.push(directory + file);
+                } else {
+                    newDirectories.push(directory + file + '\\');
+                }
+            });
+            // console.log(newDirectories);
         });
+        console.log(newDirectories)
+        directories = newDirectories;
+        readDirectory(allFiles, directories, getFiles_cb)
+    } else {
+        getFiles_cb(null, allFiles);
     }
-// }
+}
 
 /********************************************************************
- * fetchSubFiles() recieves the initial collection of files and directories
- * and continues to open sub-directories and record paths to all possible files
- * and moves the paths to those children files off of the 'directories' property of
- * the directory object onto the 'files' property with the full extension path.
+ * filesInDirectory() utilizes a publicly defined targetDirectory 
+ * then fetches all items in that directory and organizes them into
+ * two arrays one for files and one for subdirectories. An object of
+ * those arrays is passed to the next function in the waterfall.
  ********************************************************************/
-var fetchSubFiles = function (directory, next) {
+function getFiles (getFiles_cb){
     var fileArray = [];
-    var directoryArray = [];
-    var subFileArray = [];
-    var subDirectoryArray = [];
-
-    // Consider using asyncLib.series here to handle async order
-
-
-    directory.directories.map( (folder) => {
-        fs.readdir(targetDirectory + folder, function(err, files){
-            if (err){
-                getFiles_cb(err, files);
-            } else {
-                files.forEach(function(file){
-                    if(file.includes('.')) {
-                        subFileArray.push(folder + '\\' + file);
-                    } else {
-                        subDirectoryArray.push(folder + '\\'+ file);
-                    }
-                });
-            }
-            subFileArray.forEach(newFile => {directory.files.push(newFile)});
-            subFileArray = [];
-            subDirectoryArray.forEach(newDirectory => {directory.directories.push(newDirectory)});
-            subDirectoryArray = [];
-            // directory.files.forEach(thing => {console.log(thing)}) // Make sure files are being recorded correctly
-            // directory.directories.forEach(thing => {console.log(thing)}) // Make sure directories are being recorded correctly
-        });
-    });
-    // directory.files.forEach(thing => {console.log(thing)}) // Make sure files are being recorded correctly
-    // directory.directories.forEach(thing => {console.log(thing)}) // Make sure directories are being recorded correctly
+    var directoryArray = [targetDirectory];
+    readDirectory(fileArray, directoryArray, getFiles_cb);
 }
+
+
 
 /********************************************************************
  * 
@@ -81,7 +53,10 @@ var finalResults_cb = function(err, results){
     if (err) {
         console.error(err);
     } else {
-        console.log(results);
+        // console.log(results);
+        results.forEach(result => {
+            // console.log(result);
+        })
     }
 }
 
@@ -90,8 +65,7 @@ var finalResults_cb = function(err, results){
  ********************************************************************/
 function main (){
     asyncLib.waterfall([
-        filesInDirectory,
-        fetchSubFiles
+        getFiles
     ], finalResults_cb);
 }
 
